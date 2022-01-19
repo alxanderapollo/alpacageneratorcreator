5bf1056d51642962584bf7751e487f525636a505
Alex <alexsalazarny@gmail.com>
2022-01-18 22:16:11 -0500

    Added basic layout for the project

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-          Alex Was here
+    <>
+    <div className="container">
+        <div className="title">Alpaca Generator</div>
+        <div className="flex-container">
+          <div>1</div>
+          <div>2</div>
+        </div>
+        <button>Random</button>
+        <button>Download</button>
     </div>
+    </>
+   
   );
 }
 
